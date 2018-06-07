5475ee6d0b34d626ab0748f2c5948c01bf45926e
Suliman2207 <suliman1@campus.technion.ac.il>
2018-06-07 23:40:18 +0300

    Update index.php

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ GROUP BY Type1;";
         {
             while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC))
 	        {
-	            echo '<tr><td>' . $row['type_sports'] . '</td><td>' . $row['AVG_GPA'];
+	            echo '<tr><td>' . $row['type_sports'] . '</td><td>' . $row['AVG_GPA'] . '</td><td>' . $row['AVG_Weight'];
             }
         }
         else
