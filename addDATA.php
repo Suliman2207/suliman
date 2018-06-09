9c23988cd9bbd6f6a057f3716f25ac5eee59f6f7
Suliman2207 <suliman1@campus.technion.ac.il>
2018-06-09 12:09:02 +0300

    Update addDATA.php

--- addDATA.php
+++ addDATA.php
@@ -80,12 +80,16 @@ if (isset($_POST["submit"]))
 
 
                     <td><h0 style="color:#00284d;">comfort_food:</h0></td>
-                    <td><input name="comfort_food"  size="10"></td>
+                
+	<input type="text" id="comfort_food" name="comfort_food" value="<?php echo isset($_POST["comfort_food"]) ? $_POST["comfort_food"] : ''; ?>">
+
+			
+		// <td><input name="comfort_food" type="text" size="40"></td>
                     </tr>
 
                     <tr>
                         <td><h0 style="color:#00284d;">comfort_food_reasons:</h0></td>
-                        <td><input name="comfort_food_reasons" type="text" size="10"></td>
+                        <td><input name="comfort_food_reasons" type="text" size="40"></td>
                     </tr>
                     <tr>
                         <td><h0 style="color:#00284d;">Diet:</h0></td>
