0feef6877b3772138689148e6082ffc82ccf4b09
Suliman2207 <suliman1@campus.technion.ac.il>
2018-06-09 10:57:04 +0300

    Update addDATA.php

--- addDATA.php
+++ addDATA.php
@@ -17,6 +17,7 @@ $sql="SELECT count(*) FROM Food";
 $result = sqlsrv_query($conn, $sql);
 $row = sqlsrv_fetch_array($result, SQLSRV_FETCH_NUMERIC);
 $count=$row[0];
+$oo=66969;
 
 if (isset($_POST["submit"]))
 {
@@ -24,7 +25,7 @@ if (isset($_POST["submit"]))
     // First insert data to the Parts table
     $sql = "INSERT INTO Food(id, GPA, Gender, coffee, comfort_food, comfort_food_reasons, diet_current, eating_changes, 
 	fav_cuisine, food_childhood, healthy_meal, ideal_diet, meals_dinner_friend, type_sports, weight)
-    VALUES ($row[0]+1 ,".$_POST['GPA'].",".$_POST['Gender'].",".$_POST['coffee'].",".$_POST['comfort_food']."
+    VALUES ($oo ,".$_POST['GPA'].",".$_POST['Gender'].",".$_POST['coffee'].",".$_POST['comfort_food']."
     	,".$_POST['comfort_food_reasons'].",".$_POST['diet_current'].",".$_POST['eating_changes'].",".$_POST['fav_cuisine']."
 	,".$_POST['food_childhood'].",".$_POST['healthy_meal'].",".$_POST['ideal_diet'].",".$_POST['meals_dinner_friend']."
 	,".$_POST['type_sports'].",".$_POST['weight'].");";
@@ -51,7 +52,10 @@ if (isset($_POST["submit"]))
     <h1 style="color:#6666ff;">Here you can add a new Data to our world</h1></center>
 <br><br>
 <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-    
+    <table border="0" cellpadding="5">
+        <tr>
+            <td>
+                <table border="0" cellpadding="5">
 
 
                     <tr>
@@ -130,11 +134,11 @@ if (isset($_POST["submit"]))
                     <tr>
                         <td colspan="2"><button type="reset" value="Reset">Reset</button></td>
                     </tr>
-                
+                </table>
                 <p><a href="index.php" type="text/html" target="_self" style="margin-left:20px;">back</a></p>
             <td> <img src="food.jpg" alt="food" style="width:350px;height:262.5px;"></td>
         </tr>
-   
+    </table>
 </form>
 </body>
 </html>
