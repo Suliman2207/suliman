479f5d2a296cf41e1c7408300916fba05a28c81f
Suliman2207 <suliman1@campus.technion.ac.il>
2018-06-09 14:12:54 +0300

    Update addDATA.php

--- addDATA.php
+++ addDATA.php
@@ -23,10 +23,10 @@ if (isset($_POST["submit"]))
     // First insert data to the Parts table
     $sql = "INSERT INTO Food(id, GPA, Gender, coffee, comfort_food, comfort_food_reasons, diet_current, eating_changes, 
 	fav_cuisine, food_childhood, healthy_meal, ideal_diet, meals_dinner_friend, type_sports, weight)
-    VALUES ($row[0]+1,".$_POST['GPA'].",".$_POST['Gender'].",".$_POST['coffee'].",".$_POST['comfort_food']."
-    	,".$_POST['comfort_food_reasons'].",".$_POST['diet_current'].",".$_POST['eating_changes'].",".$_POST['fav_cuisine']."
-	,".$_POST['food_childhood'].",".$_POST['healthy_meal'].",".$_POST['ideal_diet'].",".$_POST['meals_dinner_friend']."
-	,".$_POST['type_sports'].",".$_POST['weight'].");";
+    VALUES ( $row[0]+1,".$_POST['GPA'].",".$_POST['Gender'].",".$_POST['coffee'].",'".$_POST['comfort_food']."'
+    	,'".$_POST['comfort_food_reasons'].",'".$_POST['diet_current'].",'".$_POST['eating_changes'].",'".$_POST['fav_cuisine']."
+	,'".$_POST['food_childhood'].",'".$_POST['healthy_meal'].",'".$_POST['ideal_diet'].",'".$_POST['meals_dinner_friend']."
+	,'".$_POST['type_sports'].",".$_POST['weight'].");";
     //echo $sql."<br>"; //debug $count++;
     $result = sqlsrv_query($conn, $sql);
     // In case of failure
@@ -63,7 +63,7 @@ if (isset($_POST["submit"]))
 
                     <tr>
                         <td><h0 style="color:#00284d;">Gender:</h0></td>
-                        <td><select name="Gender" >
+                        <td><select name="Gender" type="number" >
                                 <option value="1" selected> Male </option>
                                 <option value="2"> Female </option>
                             </select> </td>
@@ -72,7 +72,7 @@ if (isset($_POST["submit"]))
 
                     <tr>
                         <td><h0 style="color:#00284d;">coffee:</h0></td>
-                        <td><select name="coffee" >
+                        <td><select name="coffee" type="number">
                                 <option value="2" selected> Yes </option>
                                 <option value="1"> No </option>
                             </select> </td>
