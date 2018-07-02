b3aa26d255e5aa7217c841333daf3efdbb85a777
Suliman2207 <suliman1@campus.technion.ac.il>
2018-07-02 18:32:57 +0300

    Update left.php

--- left.php
+++ left.php
@@ -7,9 +7,9 @@
 <head >
     <link rel="stylesheet" href="style.css">
     <meta charset="utf-8" />
-    <title></title>
+    <title>Suliman's Website</title>
 </head>
-<body style="background-color: #edcddd;text-align: center" dir="ltr"><table style="width: 100%;text-align: center">
+<body style="background-color: #E6E6FB;text-align: center" dir="ltr"><table style="width: 100%;text-align: center">
     <tr><td>   <a href="add_file.php" target="down">Add File</a></td>
         <td>     <a href="add_report.php" target="down">Add Report</a></td>
         <td>  <a href="show_dup_companies.php" target="down">View duplicates</a></td>
