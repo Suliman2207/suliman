47d4a40ccd2f36cf1cbded7ce4eb40956b119b8c
Suliman2207 <suliman1@campus.technion.ac.il>
2018-07-02 18:17:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,12 +4,12 @@
 <head>
     <link rel="stylesheet" href="style.css">
     <meta charset="utf-8" />
-    <title></title>
+    <title>Company</title>
 </head>
 <body style="background-color: #edcddd;text-align: center" dir="ltr">
-<iframe width="100%" name="top" src="left.php" height="40"></iframe>
+<iframe width="100%" name="top" src="left.php" height="50"></iframe>
 
-<iframe  width="100%" name="down" src="main.php" height="600"></iframe>
+<iframe  width="100%" name="down" src="main.php" height="650"></iframe>
 
 
 </body>
